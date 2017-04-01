33cc8ad548dbf8c08f6a93121a55a754b1e41046
Manuel Hartmann <manuel.hartmann@mailbox.org>
2017-04-01 22:23:45 +0200

    implemented function servosTest(), to check if the servos are working

--- Sketch/MidiMassOrgel/MidiMassOrgel.ino
+++ Sketch/MidiMassOrgel/MidiMassOrgel.ino
@@ -1,54 +1,70 @@
 /* MidiMassOrgel
- by Manuel Hartmann <https://github.com/bAckmumu/swh2017a>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
+  by Manuel Hartmann <https://github.com/bAckmumu/swh2017a>
 */
 
 #include <Servo.h>
 
-int servoPin = 6;     // fisrt pin for teh servos
+int servoPin = 6;     // fisrt pin for the servos
 
 int servoCount = 8;
 
 Servo pipeServos[8];  // create an array of servo objects
 
 
-void setup() { 
-  
-  // init servos 
-  for(int i=0; i<servoCount; i++)
+void setup() {
+  // init servos
+  for (int i = 0; i < servoCount; i++)
   {
-    pipeServos[i].attach(i+servoPin);
+    pipeServos[i].attach(i + servoPin);
   }
-  
+
+  servosTest();
 }
 
 void loop() {
-//  myservo.write(0);
+  //  myservo.write(0);
+
   
-  servoTest();
 }
 
+/*
+  test function to check if all servos are working, called in setup()
+*/
+void servosTest() {
+  // 1. close pipes
+  for (int s = 0; s < servoCount; s++)
+  {
+    closePipe(s);
+  }
+
+  // 2. open pipes one by one
+  for (int s = 0; s < servoCount; s++)
+  {
+    openPipe(s);
+    delay(100);
+  }
+  delay(500);
+
+  // 3. pipes closed one by one
+  for (int s = 0; s < servoCount; s++)
+  {
+    closePipe(s);
+    delay(100);
+  }
+}
+
+/*
+  helper function to open pipe
+*/
 void openPipe(int servoNumber) {
   pipeServos[servoNumber].write(90);
 }
 
+/*
+  helper function to close pipe
+*/
 void closePipe(int servoNumber) {
   pipeServos[servoNumber].write(0);
 }
 
-void servoTest() {
-  
-   for(int s=0; s<servoCount; s++)
-   {
-      openPipe(s);
-      delay(100);
 
-      closePipe(s);
-      delay(100);
-   }
-  
-}
